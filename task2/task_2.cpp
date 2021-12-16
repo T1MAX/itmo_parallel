80ce37b6fa331a0dbcefb30ac8d2b0294ac48eb0
Dmitriy <dimaprusskii@mail.ru>
2021-12-16 18:04:48 +0300

    update to task 2

--- task2/task_2.cpp
+++ task2/task_2.cpp
@@ -28,7 +28,7 @@ void rand_init_matrix(double** matrix, int n)
 	srand(time(NULL));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			matrix[i][j] = rand() / RAND_MAX;
+			matrix[i][j] = double(rand()) / RAND_MAX;
 }
 
 // Matrix zeroing function
@@ -44,9 +44,24 @@ bool is_arrays_equal(double** first, double** second, int n)
 {
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
+        {
+//            cout << first[i][j] << " " << second[i][j] << endl;
             if(first[i][j] != second[i][j])
                 return false;
+
+        }
+
     return true;
+}
+
+void print_matrix(double** matrix, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+    }
 }
 
 int main(int argc, char** argv)
@@ -77,13 +92,15 @@ int main(int argc, char** argv)
 				C_test[i][j] += A[i][k] * B[k][j];
 	double t_ijk = omp_get_wtime() - t;
 
-    for(int n_threads = 1; n_threads <= 10; n_threads++)
+    for(int n_threads = 2; n_threads <= 10; n_threads++)
     {
+        zero_init_matrix(C, N);
+        int i, j, k;
         t = omp_get_wtime();
-        #pragma omp parallel for num_threads(n_threads)
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                    for (int k = 0; k < N; k++)
+        #pragma omp parallel for num_threads(n_threads) shared(A, B, C) private(i, j, k)
+            for (i = 0; i < N; i++)
+                for (j = 0; j < N; j++)
+                    for (k = 0; k < N; k++)
                         C[i][j] += A[i][k] * B[k][j];
         double t_ijk_parallel = omp_get_wtime() - t;
 
@@ -104,14 +121,15 @@ int main(int argc, char** argv)
 				C[i][j] += A[i][k] * B[k][j];
 	double t_jki = omp_get_wtime() - t;
 
-    for(int n_threads = 1; n_threads <= 10; n_threads++)
+    for(int n_threads = 2; n_threads <= 10; n_threads++)
     {
+        int i, j, k;
         zero_init_matrix(C, N);
         t = omp_get_wtime();
-        #pragma omp parallel for num_threads(n_threads)
-            for (int j = 0; j < N; j++)
-                for (int k = 0; k < N; k++)
-                    for (int i = 0; i < N; i++)
+        #pragma omp parallel for num_threads(n_threads) shared(A, B, C) private(i, j, k)
+            for (j = 0; j < N; j++)
+                for (k = 0; k < N; k++)
+                    for (i = 0; i < N; i++)
                         C[i][j] += A[i][k] * B[k][j];
         double t_jki_parallel = omp_get_wtime() - t;
 
@@ -132,19 +150,20 @@ int main(int argc, char** argv)
 				C[i][j] += A[i][k] * B[k][j];
 	double t_ikj = omp_get_wtime() - t;
 
-    for(int n_threads = 1; n_threads <= 10; n_threads++)
+    for(int n_threads = 2; n_threads <= 10; n_threads++)
     {
+        int i, j, k;
         zero_init_matrix(C, N);
         t = omp_get_wtime();
-        #pragma omp parallel for num_threads(n_threads)
-            for (int i = 0; i < N; i++)
-                for (int k = 0; k < N; k++)
-                    for (int j = 0; j < N; j++)
+        #pragma omp parallel for num_threads(n_threads) shared(A, B, C) private(i, j, k)
+            for (i = 0; i < N; i++)
+                for (k = 0; k < N; k++)
+                    for (j = 0; j < N; j++)
                         C[i][j] += A[i][k] * B[k][j];
         double t_ikj_parallel = omp_get_wtime() - t;
 
         if(!is_arrays_equal(C, C_test, N))
-            cout << "not equal";
+            cout << "not equal, ";
         cout << "Multiplication time with " << n_threads << " threads: " << t_ikj_parallel << " seconds, efficiency: " << t_ikj / t_ikj_parallel << endl;
     }
     cout << "------------" <<  endl;
